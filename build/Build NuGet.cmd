0a73b74d214f3ecba9ad13eb023a2274d9728087
Sergi Gisbert <sergi.gisbert@gmail.com>
2016-02-21 01:58:07 +0100

    Fix versions

--- build/Build NuGet.cmd
+++ build/Build NuGet.cmd
@@ -1,6 +1,6 @@
 
 @echo off
 "%~dp0..\tools\nuget.exe" restore "%~dp0..\Dianoga.sln"
-"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga.csproj" -Build -Symbols -Properties Configuration=Release
-"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga_8.2.csproj" -Build -Symbols -Properties Configuration=Release
+"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga_8.csproj" -Build -Symbols -Properties Configuration=Release
+"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga_9.csproj" -Build -Symbols -Properties Configuration=Release
 PAUSE
