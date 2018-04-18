52d3f7055e5373ec18ae6377fe119e51eb4c5278
Dmitry Bastron <diger74@gmail.com>
2018-04-18 17:47:27 +0500

    Kentico 10 build

--- build/Build NuGet.cmd
+++ build/Build NuGet.cmd
@@ -3,4 +3,5 @@
 "%~dp0..\tools\nuget.exe" restore "%~dp0..\Dianoga.sln"
 "%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga_8.csproj" -Build -Symbols -Properties Configuration=Kentico8
 "%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga_9.csproj" -Build -Symbols -Properties Configuration=Kentico9
+"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga_10.csproj" -Build -Symbols -Properties Configuration=Kentico10
 PAUSE
